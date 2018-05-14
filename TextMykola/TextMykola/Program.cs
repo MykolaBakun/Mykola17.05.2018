6bc1f3907a4e4ee78b3ff1d0b40370731319ebfc
MykolaCompany <36553905+MykolaCompany@users.noreply.github.com>
2018-05-14 18:50:20 +0300

    cw

--- TextMykola/TextMykola/Program.cs
+++ TextMykola/TextMykola/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace TextMykola
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mykola!!!");
+            Console.ReadKey();
         }
     }
 }
